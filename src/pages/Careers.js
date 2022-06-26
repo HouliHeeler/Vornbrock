4cd6fcadaa6c7faad4681c2d9a477c50bfc8d2f8
HouliHeeler <luke_houlihan@hotmail.com>
2022-06-26 15:26:22 -0700

    fill out Careers content

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -3,7 +3,42 @@ import React from 'react';
 const Careers = () => {
   return (
     <div>
-      <h1>Wanna work here?</h1>
+      <section>
+        <h1>Careers</h1>
+      </section>
+      <section>
+        <h4>We are hiring</h4>
+        <p>
+        R. Vornbrock is committed to creating a supportive atmosphere that facilitates growth in its team members. We firmly believe in work-life balance and an inclusive and respectful workplace. In-office, semi-remote, and remote positions are available.
+        </p>
+        <h4>Intermediate Commercial Interior Designer</h4>
+        <span>Minimum Level of Experience:</span>
+        <ul>
+          <li>3-5 years of post-graduate experience as a commercial interior designe</li>
+          <li>Education from an accredited institution in Interior Design</li>
+          <li>Strong AutoCAD skills &amp; software knowledge, including sketchUp and Microsoft Office</li>
+          <li>Strong understanding of Interior Design and Construction detailing, as well as building codes </li>
+          <li>Extensive knowledge of materials, specifications, and equipment</li>
+          <li>Strong organizational skills</li>
+          <li>Attention to detail</li>
+          <li>Excellent communication &amp; listening skills</li>
+          <li>Ability to adapt to a fast-paced environment</li>
+        </ul>
+        <h4>Interior Design AutoCAD Technician</h4>
+        <span>Minimum Level of Experience:</span>
+        <ul>
+          <li>2 years of post-graduate experience as an AutoCAD Technician in the architecture or interior design field</li>
+          <li>Education from an accredited institution in a related field</li>
+          <li>Strong AutoCAD skills &amp; software knowledge, including Microsoft Office</li>
+          <li>Strong understanding of Interior Design and Construction detailing</li>
+          <li>Strong organizational skills</li>
+          <li>Attention to detail</li>
+          <li>Excellent communication &amp; listening skills</li>
+          <li>Ability to adapt to a fast-paced environment</li>
+        </ul>
+        <span>Please forward resumes and pdf portfolios to: kate@vornbrock.com</span>
+        <span>Please note that only those selected for an interview will be contacted.</span>
+      </section>
     </div>
   );
 };
