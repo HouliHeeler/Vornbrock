1f8b31a96dd709f50d89bf295564f304e318ca0f
HouliHeeler <luke_houlihan@hotmail.com>
2022-06-26 15:07:39 -0700

    fill out homepage content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+
   
 const Home = () => {
   return (
     <div>
-      <h1>Vornbrock! Vornbrock! Vornbrock!</h1>
+      <section>
+        <h3>R.Vornbrock</h3>
+        <h1>Interior Design</h1>
+      </section>
+      <section>
+        <h4>Space Planning</h4>
+        <h4>Conceptual Design</h4>
+        <h4>Construction Documentation</h4>
+        <h4>Contract Admin</h4>
+        <p>
+        Focusing on commercial and hospitality design, R. Vornbrock &amp; Associates provides a full range of interior design services to its clients. Equally at home in the creative and technical fields, the firm is well versed in creating and executing custom design concepts for their clients. The firm also offers technical and drafting services to translate pre-existing designs into the built environment.
+        </p>
+      </section>
     </div>
   );
 };
