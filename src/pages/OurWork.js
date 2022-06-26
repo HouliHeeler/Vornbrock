c57c0aab7113ed5615fe7cebdabe2f5dd6ef1731
HouliHeeler <luke_houlihan@hotmail.com>
2022-06-26 15:12:36 -0700

    fill out Our Work content

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -3,9 +3,21 @@ import React from "react";
 const OurWork = () => {
   return (
     <div>
-      <h1>
-        Vornbrock does interior design all damn day
-      </h1>
+      <section>
+        <h1>Our Work</h1>
+      </section>
+      <section>
+        <section>
+          <img></img>
+          <span>McDonalds, Alberta</span>
+          <img></img>
+          <span>Childcare Centre, Vancouve</span>
+          <img></img>
+          <span>Walter Gage Residences, UBC</span>
+          <img></img>
+          <span>McDonalds, Manitoba</span>
+        </section>
+      </section>
     </div>
   );
 };
